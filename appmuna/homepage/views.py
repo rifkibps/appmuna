9bbdd4bc0cd25e86cbbde086fe9a8d713b41c8b4
Rifki <rifkigst.bps@gmail.com>
2024-08-09 00:12:50 +0800

    Updating project Homepage

--- appmuna/homepage/views.py
+++ appmuna/homepage/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 from app.helpers import get_highlight_dashboard_items
+from backend import models as app_model 
 
 class HomePageClassView(View):
     def get(self, request):
+
+        pubs = app_model.BackendInfographicsModel.objects.filter(show_state = '1').order_by('-updated_at').values()
         context = {
             'title' : 'Home Page',
+            'pubs' : pubs,
             'items_summarize' : get_highlight_dashboard_items()
         }
         return render(request, 'homepage/index.html', context)
